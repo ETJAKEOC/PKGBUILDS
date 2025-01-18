9a8edb1f4d6f44ad0aefb54cdc3cc15f5fd55785
ETJAKEOC <etjakeoc@gmail.com>
2025-01-18 04:25:10 -0500

    Updates

--- compile.sh
+++ compile.sh
@@ -5,12 +5,8 @@ for dir in "$AUR_DIR"/*; do
 		echo "Entering directory: $dir"
 		cd "$dir" || { echo "Failed to enter $dir"; continue; }
 		echo "Building package in $dir..."
-		mkdir src
-		sudo mount -t tmpfs tmpfs src
-		sudo chown jake:jake src
 		makepkg -s || { echo "Failed to build package in $dir"; continue; }
-		sudo umount -fl src
 		echo "Successfully built package in $dir."
 	fi
 done
-echo "All packages have been processed. You can now install them with yay -U *.pkg.tar.*"
+echo "All packages have been processed."
